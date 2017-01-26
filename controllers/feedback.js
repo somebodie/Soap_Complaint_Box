229f2f89ddbb16868114dc8c082119153f672511
s0m3b0di3 <s00m3b0di3@gmail.com>
2017-01-26 10:45:12 -0500

    sign up login and guest links all working on index

--- controllers/feedback.js
+++ controllers/feedback.js
@@ -5,6 +5,7 @@ var User = require('../models/user.js');
 var mongoose = require('mongoose');
 var Fb = require('../models/feedback.js');
 
+// TODO: http://localhost:4000/feedback
 router.get('/', function(req, res) {
   res.render('feedback/allfeedback.hbs');
 });
