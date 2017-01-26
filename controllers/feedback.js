5a5a9e98eeffad8745d5dbf7a95edbaecf148986
s0m3b0di3 <s00m3b0di3@gmail.com>
2017-01-26 15:54:00 -0500

    working on feedback post

--- controllers/feedback.js
+++ controllers/feedback.js
@@ -1,13 +1,41 @@
 var express = require('express');
 var router = express.Router();
 var User = require('../models/user.js');
-// var authHelpers = require('../helpers/auth.js')
+var authHelpers = require('../helpers/auth.js')
 var mongoose = require('mongoose');
 var Fb = require('../models/feedback.js');
 
 // TODO: http://localhost:4000/feedback
 router.get('/', function(req, res) {
   res.render('feedback/allfeedback.hbs');
+});
+
+// Feedback post
+router.get('/newpost', function(req, res) {
+  res.render('feedback/newpost.hbs');
+})
+
+router.post('/', authHelpers.authorized, function(req, res){
+  var FeedbackSchema= new Schema ({
+    subject: req.body.subject,
+    detail: req.body.detail,
+    // resolve:
+    // sort: {
+    //   postive: ,
+    //   negative:
+    // },
+    // views:
+    // created_at:
+    // updated_at:
+  });
+
+  Feedback.save(function(err, user){
+    if (err) console.log(err);
+
+    console.log(user);
+    console.log(Feedback.subject);
+    res.redirect('/users/allfeedback');
+  });
 });
 
 // Feedback UPDATE ROUTE
