6f6ab19fcc07cc78027072935c7bf46cd7b403dd
s0m3b0di3 <s00m3b0di3@gmail.com>
2017-01-26 15:27:27 -0500

    login is working

--- controllers/sessions.js
+++ controllers/sessions.js
@@ -1,15 +1,15 @@
 var express = require('express');
 var router = express.Router();
 var User = require('../models/user.js');
-// var authHelpers = require('../helpers/auth.js')
+var authHelpers = require('../helpers/auth.js')
 
 router.get('/login', function(req, res) {
   res.render('users/login.hbs')
 })
 
-// router.post('/login', authHelpers.loginUser, function(req, res){
-//   res.redirect('/users/' + req.session.currentUser._id);
-// });
+router.post('/login', authHelpers.loginUser, function(req, res){
+  res.redirect('/users/' + req.session.currentUser._id);
+});
 
 router.delete('/', function(req, res){
   req.session.destroy(function(){
