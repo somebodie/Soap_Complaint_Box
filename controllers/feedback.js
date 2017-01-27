44e960e8140b113d642b7be5f0c26fe183da6659
s0m3b0di3 <s00m3b0di3@gmail.com>
2017-01-27 14:37:22 -0500

    bug fixed

--- controllers/feedback.js
+++ controllers/feedback.js
@@ -25,39 +25,65 @@ router.get('/newpost', authHelpers.authorized, function(req, res) {
 });
 
 router.post('/', function(req, res) {
-var user = req.session.currentUser;
-    //this route will do 2 things at once:
-    //1 .Create and save Feedback to feedback collection
-    //2. Find the user this feedback belongs to and push it in that users' feedback array
-    //why would we also do option 1? this will save time to access Feedback without the user
-    // why would we do option 2? So that we can access feedback via the user
+  //Find the logged in User
+  console.log("POST ROUTE ACCESSED YAAAAAY");
 
+  User.findById(req.session.currentUser._id).exec(function(err, user) {
     var newFeedback = new Feedback({
-        subject: req.body.subject,
-        detail: req.body.detail,
-        // resolve: false,
-        type: req.params.type,
-        // views:
-    });
-
+         subject: req.body.subject,
+     });
 
     newFeedback.save(function(err, feedback) {
-        if (err) console.log(err);
-        else {
-          async.each(user.feedback, save, function(err){
-            // if any of the saves produced an error, err would equal that error
-          });
-          // user.feedback.push(feedback);
-          console.log(newPost);
-        }
-        // console.log(feedback);
-        // console.log(
-        // req.session.currentUser);
-        // console.log(feedback._id);
-      });
-          // res.redirect('feedback/' + feedback._id);
-    // });
+      console.log("FEEDBACK SAVED YAY", feedback);
+      user.feedback.push(feedback);
+
+      user.save();
+
+      console.log("USER SAVED?", user);
+
+      res.redirect('feedback/' + feedback._id);
+    })
+
+
+
+  });
 });
+
+
+// router.post('/', function(req, res) {
+// var user = req.session.currentUser;
+//     //this route will do 2 things at once:
+//     //1 .Create and save Feedback to feedback collection
+//     //2. Find the user this feedback belongs to and push it in that users' feedback array
+//     //why would we also do option 1? this will save time to access Feedback without the user
+//     // why would we do option 2? So that we can access feedback via the user
+//
+//     var newFeedback = new Feedback({
+//         subject: req.body.subject,
+//         detail: req.body.detail,
+//         // resolve: false,
+//         type: req.params.type,
+//         // views:
+//     });
+//
+//
+//     newFeedback.save(function(err, feedback) {
+//         if (err) console.log(err);
+//         // else {
+//           // async.each(user.feedback, save, function(err){
+//             // if any of the saves produced an error, err would equal that error
+//           // });
+//           user.feedback.push(feedback);
+//           console.log(newPost);
+//         // }
+//         // console.log(feedback);
+//         // console.log(
+//         // req.session.currentUser);
+//         // console.log(feedback._id);
+//       });
+//           // res.redirect('feedback/' + feedback._id);
+//     // });
+// });
 
 //Feedback Show Route
 router.get('/:id', function (req, res) {
